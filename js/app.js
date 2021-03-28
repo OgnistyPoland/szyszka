0dc573e87046592d7a060c3a45ced3f98ba1baa5
OgnistyPoland <32206254+OgnistyPoland@users.noreply.github.com>
2021-03-28 13:19:55 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -112,8 +112,6 @@ function nav(path) {
 	});
 	html += `</div></li>`;
 
-	html += `<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Bieżąca ścieżka</a><div class="dropdown-menu" aria-labelledby="navbarDropdown"><a class="dropdown-item"  href="/${cur}:/ ">> Strona główna</a>`;
-
 	if (!model.is_search_page) {
 		var arr = path.trim('/').split('/');
 		var p = '/';
